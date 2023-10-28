3ff2159d30db1e60cba8af7af57c5417b7b607eb
Joe-Chege <njorogejosephchege@gmail.com>
2023-10-28 14:04:07 +0300

    Updating get and count methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,12 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    
+    def get(self, cls, id):
+            """Implement getmethod logic"""
+            pass
+    
+    def count(self, cls=None):
+            """Implement count method logic"""
+            pass
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,10 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Implement get method logic"""
+        pass
+    def count(self, cls=None):
+        """Implement count method logic"""
+        pass
