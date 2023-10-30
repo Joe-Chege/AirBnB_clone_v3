23da443a29801d04f5d0e9a4081acd77cb8091c0
Joe-Chege <njorogejosephchege@gmail.com>
2023-10-30 03:31:53 -0700

    pycode

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,56 +1,32 @@
 #!/usr/bin/python3
-""" Flask Application """
+
+"""Imports a model"""
+
+from flask import Flask
+from os import getenv
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
+
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """ Close Storage """
-    storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Handles 404 errors.
-
-    This endpoint is triggered when a requested resource is not found.
-
-    ---
-    responses:
-      404:
-        description: A resource was not found.
-        content:
-          application/json:
-            schema:
-              type: object
-              properties:
-                error:
-                  type: string
-                  description: Error message indicating not found.
-            example:
-              error: Not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+    return {"error": "Not found"}, 404
 
 
-Swagger(app)
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    """call"""
+    storage.close()
+
 
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    """main"""
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+
     app.run(host=host, port=port, threaded=True)
