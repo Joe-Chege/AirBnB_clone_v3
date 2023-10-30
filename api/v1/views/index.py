f4c8ac47184769bddb5b95212b1ca7176e709d13
Joe-Chege <njorogejosephchege@gmail.com>
2023-10-30 03:44:59 -0700

    reversal

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-
-""" Index file """
-
-from api.v1.views import app_views
-from flask import jsonify
-from models import storage
+""" Index """
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-
-classes = {"Amenity": Amenity, "City": City, "Place": Place,
-           "Review": Review, "State": State, "User": User}
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -23,17 +18,13 @@ def status():
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def count_objects():
-    """ count the number of objects in each class"""
-    object_names = {
-            'states': 'State',
-            'cities': 'City',
-            'amenities': 'Amenity',
-            'places': 'Place',
-            'reviews': 'Review',
-            'users': 'User'
-    }
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
-    objects = {name: storage.count(classes[cls_name]) for name,
-               cls_name in object_names.items()}
-    return jsonify(objects)
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
